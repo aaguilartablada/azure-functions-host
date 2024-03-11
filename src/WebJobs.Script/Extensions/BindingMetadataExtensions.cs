63c09ce542117f9b8f2b60eb25b4ceea4cd6a667
hallvictoria <victoriahall@microsoft.com>
2024-03-11 11:18:25 -0500

    syntax

--- src/WebJobs.Script/Extensions/BindingMetadataExtensions.cs
+++ src/WebJobs.Script/Extensions/BindingMetadataExtensions.cs
@@ -1,7 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
-// _logger.LogInformation($"Worker configs: {workerConfigs}. immutableFunctionMetadata: {immutableFunctionMetadata}. functionMetadataList: {functionMetadataList}");
 
+using System;
 using Microsoft.Azure.WebJobs.Script.Description;
 
 namespace Microsoft.Azure.WebJobs.Script.Extensions
