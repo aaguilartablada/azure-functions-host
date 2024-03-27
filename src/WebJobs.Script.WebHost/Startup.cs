467fea623066d20e3e0c9fe92f4f0e36c5f05bf6
Álvaro Aguilar <alvaro.aguilar@scrm.lidl>
2024-03-27 10:16:49 +0100

    add opentelemetry to v4.27.5

--- src/WebJobs.Script.WebHost/Startup.cs
+++ src/WebJobs.Script.WebHost/Startup.cs
@@ -24,6 +24,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
             services.AddWebJobsScriptHostAuthentication();
             services.AddWebJobsScriptHostAuthorization();
             services.AddWebJobsScriptHost(Configuration);
+            services.AddPlatformOpenTelemetry(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
