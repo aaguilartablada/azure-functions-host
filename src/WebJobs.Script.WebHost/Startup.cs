3065097f2432cfa96ded105cbb6edee5ed13f86c
Álvaro Aguilar <alvaro.aguilar@scrm.lidl>
2024-03-27 14:02:39 +0100

    improve

--- src/WebJobs.Script.WebHost/Startup.cs
+++ src/WebJobs.Script.WebHost/Startup.cs
@@ -24,7 +24,28 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
             services.AddWebJobsScriptHostAuthentication();
             services.AddWebJobsScriptHostAuthorization();
             services.AddWebJobsScriptHost(Configuration);
-            services.AddPlatformOpenTelemetry(Configuration);
+
+            var config = new Dictionary<string, string>
+            {
+                { "OpenTelemetry:Exporters:Otlp:Endpoint", "http://127.0.0.1:4317"},
+                { "OpenTelemetry:Exporters:Otlp:Enabled", "true"},
+                { "OpenTelemetry:Tracing:Enabled", "false"},
+                { "OpenTelemetry:Logging:Enabled", "true"},
+                { "OpenTelemetry:Metrics:Enabled", "true"},
+                { "OpenTelemetry:Metrics:Function", "true"},
+                { "OpenTelemetry:Metrics:Sockets", "false"},
+                { "OpenTelemetry:Metrics:Custom", "false"},
+                { "OpenTelemetry:Metrics:AspNet", "false"},
+                { "OpenTelemetry:Metrics:HttpClient", "false"},
+                { "OpenTelemetry:Metrics:Runtime", "false"},
+                { "OpenTelemetry:Metrics:BuildInfo", "false"},
+                { "OpenTelemetry:Metrics:Exceptions", "false"},
+            };
+            var opentelemetryConfiguration = new ConfigurationBuilder()
+                .AddInMemoryCollection(config)
+                .AddEnvironmentVariables()
+                .Build();
+            services.AddPlatformOpenTelemetry(opentelemetryConfiguration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
